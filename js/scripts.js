538d5576f6002957a319feb3e1cf2e6f8d9533c9
simoneeferreira <simoneef1986@gmail.com>
2020-10-12 13:03:42 +0100

    week4

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$("#login").click(function(){
+    $($(this).attr("data-toggler")).modal("modal");
+})
+$("#carouselButton").click(function(){
+            if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                $("#mycarousel").carousel('pause');
+                $("#carouselButton").children("span").removeClass('fa-pause');
+                $("#carouselButton").children("span").addClass('fa-play');
+            }
+            else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                $("#mycarousel").carousel('cycle');
+                $("#carouselButton").children("span").removeClass('fa-play');
+                $("#carouselButton").children("span").addClass('fa-pause');                    
+            }
+        });
+
+$("#buttonLogin").click(function(){
+    $('#loginModal').modal('show');
+});
+
+$("#buttonReservation").click(function(){
+    $('#reservationFormModal').modal('show');
+});
